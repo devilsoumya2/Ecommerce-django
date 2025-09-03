e40fcce942ec6b73ebd98dc45b7a165df7738929
inoflact <soumyaranjanj144@gmail.com>
2025-09-03 21:59:44 +0530

    store and cart functionality

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,5 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
     path('', views.home, name='home'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
